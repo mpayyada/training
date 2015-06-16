4cde6bf03df694bb50837b8f16aa55e509144973
mpayyada <mpayyada@DIN34002020.corp.capgemini.com>
2015-06-16 17:27:47 +0530

    commit

---
+++ Assignment_1/src/TypeCast/TypeCastDemo_1.java
@@ -0,0 +1,14 @@
+package TypeCast;
+
+public class TypeCastDemo_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		    double d=88888.126;
+		    float f=(float)d;
+		    System.out.println("float num:"+f);
+			
+	}
+
+}
---
+++ Assignment_1/src/TypeCast/TypeCastDemo_2.java
@@ -0,0 +1,18 @@
+package TypeCast;
+
+public class TypeCastDemo_2 {
+	public static void main(String[] args) {
+		byte b=1;
+		short s=b;
+		int i=s;
+		long l=i;
+		float f=l;
+		double d=f;
+		System.out.println("b :"+b);
+		System.out.println("s :"+s);
+		System.out.println("i :"+i);
+		System.out.println("l :"+l);
+		System.out.println("d :"+d);
+		System.out.println("f :"+f);
+	}
+}
